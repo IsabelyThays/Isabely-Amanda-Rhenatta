8afea51236ff9d178da6450e5c998f9b4f81c8d7
IsabelyThays <146211846+IsabelyThays@users.noreply.github.com>
2024-11-26 18:59:19 -0300

    Update perguntas.js
    alterações das respostas

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,37 @@
 criaCartao(
     'Diário de Bordo', 
     'Qual foi o destino?', 
-    'Informar o destino da viagem.', 
+    'Toscana, Itália.', 
 )
 
 criaCartao(
     'Custo da Viagem', 
     'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?', 
-    'Informar o valor na moeda original do local e o valor convertido para R$. Informar se a aquisição foi via agência de viagens ou aquisição de forma independente '
+    'O euro esta atualmente custanto 6,09 reais. Escolhemos fazer nossa viagem de forma independente com um custo de R$3.590 por pessoa para 5 noites. '
 )
 
 criaCartao(
     'Fato Curioso', 
-    'Uma curiosidade sobre cidade/ponto turístico/país cisitado!', 
-    'Informar através de um breve texto uma curiosidade.'
+    'Uma curiosidade sobre cidade/ponto turístico/país visitado!', 
+    'Florença é conhecida como o berço do Renascimento.À artes muito famosas e artitas como  Leonardo da Vinci, Michelangelo, Botticelli e Donatello
 )
 
 criaCartao(
     'Passeios', 
     'Onde passear?', 
-    'Informar os locais, breve resumo do que são.'
+    'Estátua de Davi: Escultura renascentista de Michelangelo, representando o herói bíblico Davi, famosa pela perfeição anatômica e significados culturais.
+Galleria degli Uffizi: Museu de arte mundialmente conhecido, abrigando pinturas renascentistas de mestres como Botticelli e Michelangelo.
+Duomo de Florença: Catedral Santa Maria del Fiore, famosa por sua imponente cúpula de Brunelleschi, símbolo arquitetônico da cidade.'
 )
 
 criaCartao(
     'Prato Típico', 
-    'Conhece o "nome do prato"?', 
-    'Explicar sobre o prato, porque o escolheu e o ingrediente principal.'
+    'Conhece o "Cantucci"?', 
+    'O Cantucci é um biscoito tradicional da Toscana, feito com amêndoas e essência de laranja. Escolhemos este prato porque é um clássico da região, servido especialmente nos cafés da tarde, sendo uma tradição para receber visitas com elegância e sabor..'
 )
 
 criaCartao(
     'Ponto de Vista', 
     'O que eu achei da viagem para ...?', 
-    'Informar através de um breve texto sobre o que achou da viagem, o que faria de diferente e por quê.'
+    'A viagem para a Toscana foi inesquecível, com paisagens deslumbrantes, arte renascentista e culinária incrível. Cada lugar visitado teve um charme único, especialmente Florença. Se pudesse fazer algo diferente, dedicaria mais tempo aos lugares mais afastados dos pontos turístico, onde não a muitas pessoas e geralmente são lugares mais tranquilos.'
 )
