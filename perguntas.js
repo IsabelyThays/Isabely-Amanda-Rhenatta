d20dad2ec1734c1d4fe75c3f8dfa277ba1bedd7b
IsabelyThays <146211846+IsabelyThays@users.noreply.github.com>
2024-11-26 19:46:35 -0300

    Update perguntas.js
    Alteração em todas as respostas

--- perguntas.js
+++ perguntas.js
@@ -13,23 +13,25 @@ criaCartao(
 criaCartao(
     'Fato Curioso', 
     'Uma curiosidade sobre cidade/ponto turístico/país cisitado!', 
-    'Informar através de um breve texto uma curiosidade.'
+    'Florença é conhecida como o berço do Renascimento.À artes muito famosas e artitas como  Leonardo da Vinci, Michelangelo, Botticelli e Donatello.'
 )
 
 criaCartao(
     'Passeios', 
     'Onde passear?', 
-    'Informar os locais, breve resumo do que são.'
+    'Estátua de Davi: Escultura renascentista de Michelangelo, representando o herói bíblico Davi, famosa pela perfeição anatômica e significados culturais.
+'Galleria degli Uffizi: Museu de arte mundialmente conhecido, abrigando pinturas renascentistas de mestres como Botticelli e Michelangelo.
+'Duomo de Florença: Catedral Santa Maria del Fiore, famosa por sua imponente cúpula de Brunelleschi, símbolo arquitetônico da cidade.'
 )
 
 criaCartao(
     'Prato Típico', 
-    'Conhece o "nome do prato"?', 
-    'Explicar sobre o prato, porque o escolheu e o ingrediente principal.'
+    'Conhece o " Cantuccini "?', 
+    'O Cantuccini é um biscoito tradicional da Toscana, feito com amêndoas e essência de laranja. Escolhemos este prato porque é um clássico da região, servido especialmente nos cafés da tarde, sendo uma tradição para receber visitas com elegância e sabor.'
 )
 
 criaCartao(
     'Ponto de Vista', 
     'O que eu achei da viagem para ...?', 
-    'Informar através de um breve texto sobre o que achou da viagem, o que faria de diferente e por quê.'
-)
+    'O Cantucci é um biscoito tradicional da Toscana, feito com amêndoas e essência de laranja. Escolhemos este prato porque é um clássico da região, servido especialmente nos cafés da tarde, sendo uma tradição para receber visitas com elegância e sabor.
+    )
